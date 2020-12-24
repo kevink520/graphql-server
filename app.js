3d743bf2acd5373fdb817aeb17d4eb304909ee72
Kevin Kim <kevinkim75@gmail.com>
2020-12-24 16:34:52 -0500

    Made the PORT dynamic

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 
-app.listen(3000, () => console.log('App listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000'));
